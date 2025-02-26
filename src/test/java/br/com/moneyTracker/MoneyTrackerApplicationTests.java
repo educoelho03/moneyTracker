b64f7b9f98afac83a5f318044dcc48ab5d72c370
educoelho03 <eduardopaivacoelho@outlook.com>
2025-02-26 14:55:23 -0300

    save this code

---
+++ src/test/java/br/com/moneyTracker/MoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moneyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
